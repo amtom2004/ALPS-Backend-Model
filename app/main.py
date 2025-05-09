95dc721e1983d9ba6b4a349290aa0ce61d77be60
amtom2004 <amtombcs@gmail.com>
2025-05-09 23:18:21 +0530

    INIT-001 - Added all routers and logic with rainfall API

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
-from app.routers import predict, elevation, dbcheck
+from app.routers import predict, elevation, dbcheck, rainfall, displacement, lithology, slope
 
 app = FastAPI()
 
 app.include_router(predict.router)
 app.include_router(elevation.router)
 app.include_router(dbcheck.router)
+app.include_router(rainfall.router)
+app.include_router(displacement.router)
+app.include_router(lithology.router)
+app.include_router(slope.router)
 
 @app.get("/")
 def root():
--- app/model.py
+++ app/model.py
@@ -1,15 +1,26 @@
 import joblib
+import pandas as pd
 
 model = joblib.load("model/landslide_model.pkl")
 
+FEATURE_NAMES = [
+    "DISPLACEMENT (cm)",
+    "KRIGING (mm)",
+    "IDW (mm)",
+    "ELEVATION (m)",
+    "SLOPE (degrees)",
+    "LITHOLOGY"
+]
+
 def predict(input_data):
-    input_list = [
+    df = pd.DataFrame([[
         input_data.displacement,
         input_data.kriging,
         input_data.idw,
         input_data.elevation,
         input_data.slope,
         input_data.lithology
-    ]
-    prediction = model.predict([input_list])[0]
+    ]], columns=FEATURE_NAMES)
+
+    prediction = model.predict(df)[0]
     return int(prediction)
